fbe4b71c1683f3f946cecdc00bfd0ace00d93755
Raghav <raghavsahuja@gmail.com>
2019-03-18 18:11:26 +0530

    some changes in the ui

--- src/TicketRow.js
+++ src/TicketRow.js
@@ -1,41 +1,80 @@
 import React from 'react'
+import axios from 'axios'
 
 class TicketRow extends React.Component {
 
 
-    constructor(){
+    constructor() {
         super()
         this.state = {
-            visibility: 'hidden'
+            deleteTicketIcon: 'hidden',
+            statusLoading: false,
+            deletingTicket: false
         }
     }
 
     // ! Amazing 
-    mouseOver = ()=>{
-        this.setState(()=>({visibility:'visible'}))
+    mouseOver = () => {
+        this.setState(() => ({ deleteTicketIcon: 'visible' }))
     }
 
-    mouseLeave = ()=>{
-        this.setState(()=>({visibility:'hidden'}))
+    mouseLeave = () => {
+        this.setState(() => ({ deleteTicketIcon: 'hidden' }))
     }
+
+    handleStatus = (ticket, e) => {
+        const cb = e.target
+        cb.disabled = true
+
+        this.setState(()=>({statusLoading:true}))
+
+        ticket.status = ticket.status == 'open' ? 'closed' : 'open'
+        const code = ticket.ticket_code
+        axios.put(`https://dct-api-data.herokuapp.com/tickets/${code}?api_key=b441168614df4ed0`, ticket)
+            .then((response) => {
+                cb.disabled = false
+
+                this.setState(()=>({statusLoading:false}))
+                this.props.handleStatusChange(response)
+            })
+    }
+
+    handleDelete = (ticket) => {
+        const code = ticket.ticket_code
+
+        this.setState(()=>({deletingTicket:true}))
+
+        axios.delete(`https://dct-api-data.herokuapp.com/tickets/${code}?api_key=b441168614df4ed0`)
+            .then((response) => {
+                this.setState(()=>({deletingTicket:false}))
+                if (response.data.notice == 'Successfully removed the ticket') {
+                    this.props.deleteTicket(ticket)
+                }
+            })
+            .catch()
+
+    }
+
 
     render() {
         const ticket = this.props.ticket
         return (
-            <tr >
+            <tr style={this.state.deletingTicket?{background:'gray'}:{}}>
                 <td class="pointer" onDoubleClick={(e) => {
-                    this.props.handleDelete(ticket)
+                    this.handleDelete(ticket)
                 }} onMouseOver={this.mouseOver} onMouseLeave={this.mouseLeave}>
                     {ticket.ticket_code + ' '}
-                    <i style={{ visibility: this.state.visibility }} class="fa fa-remove"></i>
+                    <i style={{ visibility: this.state.deleteTicketIcon }} class="fa fa-remove"></i>
                 </td>
                 <td>{ticket.name}</td>
                 <td>{ticket.department}</td>
                 <td>{ticket.priority}</td>
                 <td>{ticket.message}</td>
                 <td><input id={ticket.id} type="checkbox" onChange={(e) => {
-                    this.props.handleStatus(ticket, e)
-                }} checked={ticket.status == 'open' ? false : true} /></td>
+                    this.handleStatus(ticket, e)
+                }} checked={ticket.status == 'open' ? false : true} />
+                    <i style={{visibility:this.state.statusLoading || this.state.deletingTicket ?'visible':'hidden'}} class="fa fa-spinner fa-spin m-2"></i>
+                </td>
             </tr>
         )
     }
--- src/TicketTable.js
+++ src/TicketTable.js
@@ -4,35 +4,7 @@ import axios from 'axios';
 import TicketRow from './TicketRow'
 
 class TicketTable extends React.Component {
-
-    handleDelete = (ticket) => {
-
-        const code = ticket.ticket_code
-        axios.delete(`https://dct-api-data.herokuapp.com/tickets/${code}?api_key=b441168614df4ed0`)
-            .then((response) => {
-                if (response.data.notice == 'Successfully removed the ticket') {
-                    this.props.deleteTicket(ticket)
-                }
-            })
-            .catch()
-
-    }
-
-    handleStatus = (ticket, e) => {
-        const cb = e.target
-        cb.disabled = true
-        cb.style.cursor = 'progress'
-
-        ticket.status = ticket.status == 'open' ? 'closed' : 'open'
-        const code = ticket.ticket_code
-        axios.put(`https://dct-api-data.herokuapp.com/tickets/${code}?api_key=b441168614df4ed0`, ticket)
-            .then((response) => {
-                cb.disabled = false
-                cb.style.cursor = 'default'
-                this.props.handleStatusChange(response)
-            })
-    }
-
+  
     render() {
         return (
             <form class="col-9">
@@ -52,7 +24,8 @@ class TicketTable extends React.Component {
                                     <TicketRow
                                         ticket={ticket}
                                         handleDelete={this.handleDelete}
-                                        handleStatus={this.handleStatus}
+                                        handleStatusChange={this.props.handleStatusChange}
+                                        deleteTicket={this.props.deleteTicket}
                                     />
                                 )
                             })
